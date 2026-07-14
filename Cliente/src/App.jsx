085c66c2b9bc84948af16948079f2072a58f778a
Agus20022002 <agustineguzman2016@gmail.com>
2026-07-14 19:16:02 -0300

    cambios a admin inicio

--- Cliente/src/App.jsx
+++ Cliente/src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PaginaInicio from './Paginas/Pagina_Inicio';
 import Login from './Paginas/Login';
 import Admin_Inicio from './Paginas/Admin_Inicio';
+import Op_Inicio from './Paginas/Op_Inicio';
 
 // Estos imports los dejo como guía, no son todos, pero es una vista general de lo que ocuparemos
 //import RegisterFormPage from './Pages/RegisterFormPage';
@@ -43,7 +44,8 @@ function App() {
     <Routes>
       <Route path="/" element={<PaginaInicio />} />
       <Route path="/Login" element={<Login />} />
-      <Route path="/Admin" element={<Admin_Inicio />} />
+      <Route path="/Admin_Inicio" element={<Admin_Inicio />} />
+      <Route path="/Op_Inicio" element={<Op_Inicio />} />
 
       {/* Enrutamientos a ajustar para poder usarlos*/}
 {/*      <Route path="/RegisterForm" element={<RegisterFormPage />} />
