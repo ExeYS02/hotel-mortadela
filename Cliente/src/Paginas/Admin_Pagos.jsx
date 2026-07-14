242a76c5c40dd6c71020b8df03baf6c4476338cb
Agus20022002 <agustineguzman2016@gmail.com>
2026-07-14 19:36:24 -0300

    cambios a archivos de admin 2

---
+++ Cliente/src/Paginas/Admin_Pagos.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Admin_Pagos() {
+    return (
+        <div>
+            <header>
+                <h1>Esta es la pag de inicio de los pagos</h1>
+                <p>Recordar que este un es un header temporal, por que teniamos planeado </p>
+                </header>
+                </div>
+    )
+}
+
+export default Admin_Pagos;
